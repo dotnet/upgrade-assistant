848fbaec3c3661890d4cd129d817a4b7eff3e505
Mike Rousos <Mike@MJRousos.com>
2020-10-02 14:59:54 -0400

    Add initial projects

---
+++ src/AspNetMigrator.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AspNetMigrator.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
