f04db6719666703b3440169d14eb7c7173106b37
Mike Rousos <Mike@MJRousos.com>
2021-02-10 11:32:55 -0500

    Fix PackageCollection to not fail on duplicate package references

--- src/steps/Microsoft.UpgradeAssistant.Steps.Packages/PackageCollection.cs
+++ src/steps/Microsoft.UpgradeAssistant.Steps.Packages/PackageCollection.cs
@@ -10,9 +10,19 @@ namespace Microsoft.UpgradeAssistant.Steps.Packages
     {
         private readonly Dictionary<string, NuGetReference> _packages;
 
+        public Dictionary<string, IEnumerable<NuGetReference>> DuplicatePackages { get; }
+
         public PackageCollection(IEnumerable<NuGetReference> packages)
         {
-            _packages = packages.ToDictionary(p => p.Name, StringComparer.OrdinalIgnoreCase);
+            // In the rare case a package is referenced more than once, only track the highest version
+            var filteredPackages = packages.Where(p => !packages.Any(p1 => p1.Name.Equals(p.Name, StringComparison.OrdinalIgnoreCase) && p1.GetNuGetVersion() > p.GetNuGetVersion()));
+            _packages = filteredPackages.ToDictionary(p => p.Name, StringComparer.OrdinalIgnoreCase);
+
+            // Track packages with multiple versions referenced so that analyzers can evaluate them
+            DuplicatePackages = packages.Where(p => packages.Any(p1 => p1.Name.Equals(p.Name, StringComparison.OrdinalIgnoreCase) && p1.GetNuGetVersion() > p.GetNuGetVersion()))
+                .Select(p => p.Name)
+                .Distinct()
+                .ToDictionary(n => n, n => packages.Where(p => p.Name.Equals(n, StringComparison.OrdinalIgnoreCase)));
         }
 
         public bool TryGetPackageByName(string packageName, [MaybeNullWhen(false)] out NuGetReference nugetReference)
