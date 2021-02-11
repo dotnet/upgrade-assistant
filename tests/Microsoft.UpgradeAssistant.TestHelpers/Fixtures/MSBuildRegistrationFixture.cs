d5f04b2cb935d5f5a16852428bca041f08b62a46
Mike Rousos <Mike@MJRousos.com>
2021-02-11 13:33:13 -0500

    Only restore projects for tests that need them restored

--- tests/Microsoft.UpgradeAssistant.TestHelpers/Fixtures/MSBuildRegistrationFixture.cs
+++ tests/Microsoft.UpgradeAssistant.TestHelpers/Fixtures/MSBuildRegistrationFixture.cs
@@ -7,31 +7,11 @@ namespace Microsoft.UpgradeAssistant.Fixtures
 {
     public class MSBuildRegistrationFixture
     {
-        private static readonly string[] ProjectsToRestore = new[]
-        {
-            @"..\..\..\..\..\tests\TestAssets\TestProject\TestProject.csproj"
-        };
-
         public MSBuildRegistrationFixture()
         {
             // Register MSBuild
             var msBuildRegistrar = new MSBuildRegistrationStartup(new NullLogger<MSBuildRegistrationStartup>());
             msBuildRegistrar.RegisterMSBuildInstance();
-
-            foreach (var project in ProjectsToRestore)
-            {
-                EnsurePackagesRestored(Environment.ExpandEnvironmentVariables(project));
-            }
-        }
-
-        private static void EnsurePackagesRestored(string? projectPath)
-        {
-            if (projectPath is not null)
-            {
-                var project = new ProjectInstance(projectPath);
-                var restorer = new MSBuildPackageRestorer(new NullLogger<MSBuildPackageRestorer>());
-                restorer.RestorePackages(project);
-            }
         }
     }
 }
