f08b04b31ca0d47685fe1dc86d5b64c51a52c385
Taylor Southwick <tasou@microsoft.com>
2021-02-19 12:29:32 -0800

    Revert "Move some mocks"
    This reverts commit dada9a8c537ecbcd07b9706475e33ac180609539.

---
+++ tests/Microsoft.DotNet.UpgradeAssistant.TestHelpers/Defaults.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.IO;
+
+namespace Microsoft.DotNet.UpgradeAssistant
+{
+    public static class Defaults
+    {
+        public static MigrateOptions DefaultMigrateOptions =>
+            new()
+            {
+                // Migrate options are only valid if their Project property points at a file that exists
+                Project = new FileInfo(typeof(Defaults).Assembly.Location)
+            };
+    }
+}
