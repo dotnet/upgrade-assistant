2d78a1cd583afc6180d75f34b1a0643966e2c394
Taylor Southwick <tasou@microsoft.com>
2021-02-26 13:29:22 -0800

    Add a way to verify upgrade readiness
    This adds an interface IUpgradeReadyCheck that will be run at initialization to allow easy checks to block known problematic configurations. An initial check to block on CentralPackageVersion is added while we investigate adding support for this.

--- src/components/Microsoft.DotNet.UpgradeAssistant/UpgraderExtensions.cs
+++ src/components/Microsoft.DotNet.UpgradeAssistant/UpgraderExtensions.cs
@@ -1,6 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using Microsoft.DotNet.UpgradeAssistant.Checks;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Microsoft.DotNet.UpgradeAssistant
@@ -11,6 +12,12 @@ namespace Microsoft.DotNet.UpgradeAssistant
         {
             services.AddScoped<UpgraderManager>();
             services.AddTransient<IUpgradeStepOrderer, UpgradeStepOrderer>();
+            services.AddReadinessChecks();
+        }
+
+        public static void AddReadinessChecks(this IServiceCollection services)
+        {
+            services.AddTransient<IUpgradeReadyCheck, CentralPackageManagementCheck>();
         }
     }
 }
