62c0ee988f88d9a2ffaf4a8f1da8633aa5692cb9
Mike Rousos <Mike@MJRousos.com>
2021-02-26 17:36:47 -0500

    Add a readiness check that confirms input projects have valid TFMs

---
+++ src/components/Microsoft.DotNet.UpgradeAssistant/Checks/MultiTargetingCheck.cs
@@ -0,0 +1,48 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.DotNet.UpgradeAssistant.Checks
+{
+    public class MultiTargetingCheck : IUpgradeReadyCheck
+    {
+        private readonly ILogger<MultiTargetingCheck> _logger;
+
+        public MultiTargetingCheck(ILogger<MultiTargetingCheck> logger)
+        {
+            _logger = logger;
+        }
+
+        public string Id => nameof(MultiTargetingCheck);
+
+        public Task<bool> IsReadyAsync(IUpgradeContext context, CancellationToken token)
+        {
+            if (context is null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            foreach (var project in context.Projects)
+            {
+                var projectRoot = project.GetFile();
+
+                try
+                {
+                    var tfm = project.TFM;
+                    _logger.LogTrace("Confirmed project {Project} has a valid TFM ({TFM})", project.FilePath, tfm);
+                }
+                catch (UpgradeException)
+                {
+                    _logger.LogCritical("Project {Project} cannot be upgraded. Input projects must have exactly one <TargetFramework> or <TargetFrameworkVersion> property. Multi-targeted projects are not yet supported.", project.FilePath);
+                    return Task.FromResult(false);
+                }
+            }
+
+            return Task.FromResult(true);
+        }
+    }
+}
---
+++ tests/components/Microsoft.DotNet.UpgradeAssistant.Tests/Checks/MultiTargetingCheckTests.cs
@@ -0,0 +1,60 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.DotNet.UpgradeAssistant.Checks;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using Xunit;
+
+namespace Microsoft.DotNet.UpgradeAssistant.Tests.Checks
+{
+    public class MultiTargetingCheckTests
+    {
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public async Task OnlyValidTFMsPassCheck(IProject[] projects, bool isReady)
+        {
+            // Arrange
+            var context = new Mock<IUpgradeContext>();
+            context.Setup(c => c.Projects).Returns(projects);
+
+            var readyCheck = new MultiTargetingCheck(NullLogger<MultiTargetingCheck>.Instance);
+
+            // Act
+            var result = await readyCheck.IsReadyAsync(context.Object, CancellationToken.None).ConfigureAwait(false);
+
+            // Assert
+            Assert.Equal(isReady, result);
+        }
+
+        public static IEnumerable<object[]> TestData =>
+            new List<object[]>
+            {
+                    new object[] { new IProject[] { GetValidProject() }, true },
+                    new object[] { new IProject[] { GetValidProject(), GetValidProject() }, true },
+                    new object[] { new IProject[] { GetInvalidProject() }, false },
+                    new object[] { new IProject[] { GetValidProject(), GetInvalidProject(), GetValidProject() }, false },
+                    new object[] { Array.Empty<IProject>(), true },
+            };
+
+        private static IProject GetValidProject()
+        {
+            var project = new Mock<IProject>();
+            project.Setup(p => p.TFM).Returns(new TargetFrameworkMoniker("net5.0"));
+
+            return project.Object;
+        }
+
+        private static IProject GetInvalidProject()
+        {
+            var project = new Mock<IProject>();
+            project.Setup(p => p.TFM).Throws<UpgradeException>();
+
+            return project.Object;
+        }
+    }
+}
