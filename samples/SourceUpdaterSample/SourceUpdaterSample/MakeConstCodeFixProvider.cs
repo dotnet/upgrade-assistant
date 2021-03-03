d34994f1d0ac84551b8ab4584eda2f0d6cab0800
Mike Rousos <Mike@MJRousos.com>
2021-03-03 10:27:29 -0500

    Add sample source analyzer extension

---
+++ samples/SourceUpdaterSample/SourceUpdaterSample/MakeConstCodeFixProvider.cs
@@ -0,0 +1,93 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
+
+namespace SourceUpdaterSample
+{
+    /// <summary>
+    /// As with the analzyers, code fix providers that are registered into Upgrade Assistant's
+    /// dependency injection container (by IExtensionServiceProvider) will be used during
+    /// the source update step.
+    /// </summary>
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = "UA001 CodeFix Provider")]
+    public class MakeConstCodeFixProvider : CodeFixProvider
+    {
+        // The Upgrade Assistant will only use analyzers that have an associated code fix provider registered including
+        // the analyzer's ID in the code fix provider's FixableDiagnosticIds array.
+        public sealed override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(MakeConstAnalyzer.DiagnosticId);
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            if (root is null)
+            {
+                return;
+            }
+
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            var declaration = root.FindToken(diagnosticSpan.Start).Parent?.AncestorsAndSelf().OfType<LocalDeclarationStatementSyntax>().First();
+
+            if (declaration is null)
+            {
+                return;
+            }
+
+            // Register a code action that will invoke the fix.
+            context.RegisterCodeFix(
+                CodeAction.Create(
+                    Resources.MakeConstTitle,
+                    c => MakeConstAsync(context.Document, declaration, c),
+                    nameof(Resources.MakeConstTitle)),
+                diagnostic);
+        }
+
+        private static async Task<Document> MakeConstAsync(Document document, LocalDeclarationStatementSyntax localDeclaration, CancellationToken cancellationToken)
+        {
+            // Remove the leading trivia from the local declaration.
+            var firstToken = localDeclaration.GetFirstToken();
+            var leadingTrivia = firstToken.LeadingTrivia;
+            var trimmedLocal = localDeclaration.ReplaceToken(
+                firstToken, firstToken.WithLeadingTrivia(SyntaxTriviaList.Empty));
+
+            // Create a const token with the leading trivia.
+            var constToken = SyntaxFactory.Token(leadingTrivia, SyntaxKind.ConstKeyword, SyntaxFactory.TriviaList(SyntaxFactory.ElasticMarker));
+
+            // Insert the const token into the modifiers list, creating a new modifiers list.
+            var newModifiers = trimmedLocal.Modifiers.Insert(0, constToken);
+
+            // Produce the new local declaration.
+            var newLocal = trimmedLocal
+                .WithModifiers(newModifiers)
+                .WithDeclaration(localDeclaration.Declaration);
+
+            // Add an annotation to format the new local declaration.
+            var formattedLocal = newLocal.WithAdditionalAnnotations(Formatter.Annotation);
+
+            // Replace the old local declaration with the new local declaration.
+            var oldRoot = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+            var newRoot = oldRoot!.ReplaceNode(localDeclaration, formattedLocal);
+
+            // Return document with transformed tree.
+            return document.WithSyntaxRoot(newRoot);
+        }
+    }
+}
