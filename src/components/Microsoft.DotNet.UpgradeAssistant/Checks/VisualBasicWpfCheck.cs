9899ace46c6d797ed63b3b2715901aea26b7bafe
Ken Schlobohm <keschlob@microsoft.com>
2021-03-03 17:37:39 -0600

    added ready check for Visual Basic WPF

---
+++ src/components/Microsoft.DotNet.UpgradeAssistant/Checks/VisualBasicWpfCheck.cs
@@ -0,0 +1,45 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.DotNet.UpgradeAssistant.Checks
+{
+    public class VisualBasicWpfCheck : IUpgradeReadyCheck
+    {
+        private readonly ILogger<MultiTargetingCheck> _logger;
+
+        public VisualBasicWpfCheck(ILogger<MultiTargetingCheck> logger)
+        {
+            _logger = logger;
+        }
+
+        public string Id => nameof(VisualBasicWpfCheck);
+
+        public Task<bool> IsReadyAsync(IUpgradeContext context, CancellationToken token)
+        {
+            if (context is null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            foreach (var project in context.Projects)
+            {
+                var projectRoot = project.GetFile();
+
+                if (project.Language == Languages.VisualBasic && project.Components.HasFlag(ProjectComponents.WPF))
+                {
+                    _logger.LogCritical("Project {Project} cannot be upgraded. try-convert version 0.7.212201 does not support the migration of Visual Basic WPF applications", project.FilePath);
+                }
+            }
+
+            return Task.FromResult(false);
+        }
+    }
+}
