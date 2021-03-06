57a577d9e0fa359b3ccdf1e791a870c5e05d67fa
Sunanda Balasubramanian <subal@microsoft.com>
2021-03-05 16:03:51 -0800

    Remove unnecessary imports

--- src/steps/Microsoft.DotNet.UpgradeAssistant.Steps.ProjectFormat/TryConvertToolStartup.cs
+++ src/steps/Microsoft.DotNet.UpgradeAssistant.Steps.ProjectFormat/TryConvertToolStartup.cs
@@ -1,8 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Linq;
-using System.Runtime.Loader;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
