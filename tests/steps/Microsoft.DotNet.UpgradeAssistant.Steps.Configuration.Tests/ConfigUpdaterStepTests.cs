8cce8efb3a8d17f95fe31968f425549a9dd097e7
Mike Rousos <Mike@MJRousos.com>
2021-03-08 10:09:12 -0500

    Scaffold ConfigUpdaterStep tests

---
+++ tests/steps/Microsoft.DotNet.UpgradeAssistant.Steps.Configuration.Tests/ConfigUpdaterStepTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace Microsoft.DotNet.UpgradeAssistant.Steps.Configuration.Tests
+{
+    public class ConfigUpdaterStepTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
