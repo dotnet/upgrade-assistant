22758c752170efd001b18e371082ffd5d2482630
Ken Schlobohm <keschlob@microsoft.com>
2021-03-10 16:59:40 -0600

    now registered for DI

--- src/extensions/default/Microsoft.DotNet.UpgradeAssistant.Extensions.Default/DefaultExtensionServiceProvider.cs
+++ src/extensions/default/Microsoft.DotNet.UpgradeAssistant.Extensions.Default/DefaultExtensionServiceProvider.cs
@@ -62,6 +62,7 @@ namespace Microsoft.DotNet.UpgradeAssistant.Extensions.Default
             // Add source analyzers and code fix providers (note that order doesn't matter as they're run alphabetically)
             // Analyzers
             services.AddTransient<DiagnosticAnalyzer, AllowHtmlAttributeAnalyzer>();
+            services.AddTransient<DiagnosticAnalyzer, BinaryFormatterUnsafeDeserializeAnalyzer>();
             services.AddTransient<DiagnosticAnalyzer, FilterAnalyzer>();
             services.AddTransient<DiagnosticAnalyzer, HelperResultAnalyzer>();
             services.AddTransient<DiagnosticAnalyzer, HtmlHelperAnalyzer>();
@@ -74,6 +75,7 @@ namespace Microsoft.DotNet.UpgradeAssistant.Extensions.Default
 
             // Code fix providers
             services.AddTransient<CodeFixProvider, AllowHtmlAttributeCodeFixer>();
+            services.AddTransient<CodeFixProvider, BinaryFormatterUnsafeDeserializeFixer>();
             services.AddTransient<CodeFixProvider, FilterCodeFixer>();
             services.AddTransient<CodeFixProvider, HelperResultCodeFixer>();
             services.AddTransient<CodeFixProvider, HtmlHelperCodeFixer>();
