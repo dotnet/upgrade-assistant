caef78e1ff20530b1b730eaeaddf358b15949536
Mike Rousos <mikerou@microsoft.com>
2021-03-11 14:47:37 -0500

    Add a command to select a different current project (#343)
    Fixes #338

---
+++ src/components/Microsoft.DotNet.UpgradeAssistant/Commands/SelectProjectCommand.cs
@@ -0,0 +1,26 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.DotNet.UpgradeAssistant.Commands
+{
+    public class SelectProjectCommand : UpgradeCommand
+    {
+        public override string CommandText => "Select different project";
+
+        public override Task<bool> ExecuteAsync(IUpgradeContext context, CancellationToken token)
+        {
+            if (context is null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            context.SetCurrentProject(null);
+
+            return Task.FromResult(true);
+        }
+    }
+}
