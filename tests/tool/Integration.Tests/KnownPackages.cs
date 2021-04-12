7991d033ed7b07c867a06a761deb84f191d09716
Ken Schlobohm <keschlob@microsoft.com>
2021-04-12 09:36:19 -0500

    added comments

--- tests/tool/Integration.Tests/KnownPackages.cs
+++ tests/tool/Integration.Tests/KnownPackages.cs
@@ -10,11 +10,17 @@ using Microsoft.DotNet.UpgradeAssistant;
 
 namespace Integration.Tests
 {
+    /// <summary>
+    /// Used for testing, this class provides a place to define the data that overrides details provided by NuGet.
+    /// </summary>
     internal class KnownPackages
     {
         private const string EXPECTED_PACKAGE_VERSIONS = "ExpectedPackageVersions.json";
         private readonly Dictionary<string, string> _knownValues;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="KnownPackages"/> class.
+        /// </summary>
         public KnownPackages()
         {
             var knownVersionsJson = File.ReadAllText(EXPECTED_PACKAGE_VERSIONS);
@@ -22,6 +28,12 @@ namespace Integration.Tests
                 ?? throw new InvalidOperationException($"{EXPECTED_PACKAGE_VERSIONS} was not found");
         }
 
+        /// <summary>
+        /// Gets the NuGetReference data for a package that has been overriden, or returns false.
+        /// </summary>
+        /// <param name="name">name of the NuGet package.</param>
+        /// <param name="nuget">a <see cref="NuGetReference"/>.</param>
+        /// <returns>true if NuGetReference is populated, otherwise false.</returns>
         public bool TryGetValue(string name, [MaybeNullWhen(false)] out NuGetReference nuget)
         {
             if (_knownValues is null)
