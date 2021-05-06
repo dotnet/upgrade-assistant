15f9e44efbf806e8fdd2b4406b4075474e027e71
Mike Rousos <Mike@MJRousos.com>
2021-05-06 09:42:31 -0400

    WIP: NuGet credential update

--- src/components/Microsoft.DotNet.UpgradeAssistant.MSBuild/NuGetCredentialsStartup.cs
+++ src/components/Microsoft.DotNet.UpgradeAssistant.MSBuild/NuGetCredentialsStartup.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -17,11 +18,18 @@ namespace Microsoft.DotNet.UpgradeAssistant.MSBuild
 {
     public class NuGetCredentialsStartup : IUpgradeStartup
     {
+        private readonly ISettings _nugetSettings;
         private readonly IUserInput _userInput;
         private readonly ILogger<NuGetCredentialsStartup> _logger;
 
-        public NuGetCredentialsStartup(IUserInput userInput, ILogger<NuGetCredentialsStartup> logger)
+        public NuGetCredentialsStartup(UpgradeOptions upgradeOptions, IUserInput userInput, ILogger<NuGetCredentialsStartup> logger)
         {
+            if (upgradeOptions is null)
+            {
+                throw new ArgumentNullException(nameof(upgradeOptions));
+            }
+
+            _nugetSettings = Settings.LoadDefaultSettings(Path.GetDirectoryName(upgradeOptions.ProjectPath));
             _userInput = userInput ?? throw new ArgumentNullException(nameof(userInput));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
@@ -31,11 +39,12 @@ namespace Microsoft.DotNet.UpgradeAssistant.MSBuild
             if (HttpHandlerResourceV3.CredentialService == null)
             {
                 _logger.LogDebug("Registering NuGet credential providers");
-                HttpHandlerResourceV3.CredentialService = new Lazy<ICredentialService>(
-                    () => new CredentialService(
+                var credentialService = new CredentialService(
                         providers: new AsyncLazy<IEnumerable<ICredentialProvider>>(async () => await GetCredentialProvidersAsync().ConfigureAwait(false)),
                         nonInteractive: !_userInput.IsInteractive,
-                        handlesDefaultCredentials: PreviewFeatureSettings.DefaultCredentialsAfterCredentialProviders));
+                        handlesDefaultCredentials: PreviewFeatureSettings.DefaultCredentialsAfterCredentialProviders);
+
+                HttpHandlerResourceV3.CredentialService = new Lazy<ICredentialService>(() => credentialService);
             }
 
             return Task.FromResult(true);
@@ -43,13 +52,22 @@ namespace Microsoft.DotNet.UpgradeAssistant.MSBuild
 
         private async Task<IEnumerable<ICredentialProvider>> GetCredentialProvidersAsync()
         {
-            var pluginProviderBuilder = new SecurePluginCredentialProviderBuilder(PluginManager.Instance, false, new NuGetLogger(_logger));
-            var pluginProviders = (await pluginProviderBuilder.BuildAllAsync().ConfigureAwait(false)).ToList();
+            // TODO : Expand to be more like https://github.com/NuGet/NuGet.Client/blob/17c4f841ff61d27fe6b57cf45ceef16037062635/src/NuGet.Clients/NuGet.CommandLine/Commands/Command.cs#L209
+            var logger = new NuGetLogger(_logger);
+            var ret = new List<ICredentialProvider>();
 
-            if (PreviewFeatureSettings.DefaultCredentialsAfterCredentialProviders)
+            var pluginProviders = new PluginCredentialProviderBuilder(null! /* TODO */, _nugetSettings, logger).BuildAll(NuGetVerbosity.Normal);
+            ret.AddRange(pluginProviders);
+
+            var securePluginProviders = new SecurePluginCredentialProviderBuilder(PluginManager.Instance, true, new NuGetLogger(_logger));
+            ret.AddRange(await securePluginProviders.BuildAllAsync().ConfigureAwait(false));
+
+            if (ret.Any() && PreviewFeatureSettings.DefaultCredentialsAfterCredentialProviders)
             {
-                pluginProviders.Add(new DefaultNetworkCredentialsCredentialProvider());
+                ret.Add(new DefaultNetworkCredentialsCredentialProvider());
             }
+
+            ret.Add(new ConsoleCredentialProvider(logger));
 
             _logger.LogDebug("Using NuGet credential providers: {Providers}", string.Join(", ", pluginProviders.Select(p => p.Id)));
 
---
+++ src/components/Microsoft.DotNet.UpgradeAssistant.MSBuild/NuGetVerbosity.cs
@@ -0,0 +1,12 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.DotNet.UpgradeAssistant.MSBuild
+{
+    public static class NuGetVerbosity
+    {
+        public const string Normal = "Normal";
+        public const string Quit = "Quiet";
+        public const string Detailed = "Detailed";
+    }
+}
