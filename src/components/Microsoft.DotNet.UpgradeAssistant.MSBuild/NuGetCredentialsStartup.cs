b4f5e6cd61d981d4c79ac6df2b42abfe00090468
Mike Rousos <Mike@MJRousos.com>
2021-05-07 17:19:12 -0400

    Simplify NuGetCredentialsStartup

--- src/components/Microsoft.DotNet.UpgradeAssistant.MSBuild/NuGetCredentialsStartup.cs
+++ src/components/Microsoft.DotNet.UpgradeAssistant.MSBuild/NuGetCredentialsStartup.cs
@@ -2,16 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
-using NuGet.Common;
-using NuGet.Configuration;
 using NuGet.Credentials;
-using NuGet.Protocol;
-using NuGet.Protocol.Plugins;
 
 namespace Microsoft.DotNet.UpgradeAssistant.MSBuild
 {
@@ -28,32 +22,8 @@ namespace Microsoft.DotNet.UpgradeAssistant.MSBuild
 
         public Task<bool> StartupAsync(CancellationToken token)
         {
-            if (HttpHandlerResourceV3.CredentialService == null)
-            {
-                _logger.LogDebug("Registering NuGet credential providers");
-                HttpHandlerResourceV3.CredentialService = new Lazy<ICredentialService>(
-                    () => new CredentialService(
-                        providers: new AsyncLazy<IEnumerable<ICredentialProvider>>(async () => await GetCredentialProvidersAsync().ConfigureAwait(false)),
-                        nonInteractive: !_userInput.IsInteractive,
-                        handlesDefaultCredentials: PreviewFeatureSettings.DefaultCredentialsAfterCredentialProviders));
-            }
-
+            DefaultCredentialServiceUtility.SetupDefaultCredentialService(new NuGetLogger(_logger), !_userInput.IsInteractive);
             return Task.FromResult(true);
-        }
-
-        private async Task<IEnumerable<ICredentialProvider>> GetCredentialProvidersAsync()
-        {
-            var pluginProviderBuilder = new SecurePluginCredentialProviderBuilder(PluginManager.Instance, false, new NuGetLogger(_logger));
-            var pluginProviders = (await pluginProviderBuilder.BuildAllAsync().ConfigureAwait(false)).ToList();
-
-            if (PreviewFeatureSettings.DefaultCredentialsAfterCredentialProviders)
-            {
-                pluginProviders.Add(new DefaultNetworkCredentialsCredentialProvider());
-            }
-
-            _logger.LogDebug("Using NuGet credential providers: {Providers}", string.Join(", ", pluginProviders.Select(p => p.Id)));
-
-            return pluginProviders;
         }
     }
 }
