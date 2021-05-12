975c87f7fdc31bd41969ecef0dd147808e975a01
Taylor Southwick <tasou@microsoft.com>
2021-05-12 16:43:43 -0700

    Fix up from main

--- src/common/Microsoft.DotNet.UpgradeAssistant.Abstractions/Extensions/IExtensionServiceProvider.cs
+++ src/common/Microsoft.DotNet.UpgradeAssistant.Abstractions/Extensions/IExtensionServiceProvider.cs
@@ -1,7 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Microsoft.DotNet.UpgradeAssistant.Packages;
+using Microsoft.DotNet.UpgradeAssistant.Dependencies;
 
 namespace Microsoft.DotNet.UpgradeAssistant.Extensions
 {
