36b3b634efca31fe1b61dfd603722958d8280dbb
Taylor Southwick <tasou@microsoft.com>
2021-06-02 12:53:51 -0700

    Ensure typemap is copied

--- src/steps/Microsoft.DotNet.UpgradeAssistant.Steps.Configuration/ConfigUpdaterOptions.cs
+++ src/steps/Microsoft.DotNet.UpgradeAssistant.Steps.Configuration/ConfigUpdaterOptions.cs
@@ -1,10 +1,12 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
+
 namespace Microsoft.DotNet.UpgradeAssistant.Steps.Configuration
 {
     public class ConfigUpdaterOptions
     {
-        public string[]? ConfigFilePaths { get; set; }
+        public string[] ConfigFilePaths { get; set; } = Array.Empty<string>();
     }
 }
