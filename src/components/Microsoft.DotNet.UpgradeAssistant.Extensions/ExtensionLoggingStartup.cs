5858d49ca2354ef3cd3ab72ea2b145a5e62a0f72
Taylor Southwick <tasou@microsoft.com>
2021-06-01 20:40:19 -0700

    Log extensions that were loaded (#572)

---
+++ src/components/Microsoft.DotNet.UpgradeAssistant.Extensions/ExtensionLoggingStartup.cs
@@ -0,0 +1,42 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Loader;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.DotNet.UpgradeAssistant.Extensions
+{
+    internal class ExtensionLoggingStartup : IUpgradeStartup
+    {
+        private readonly ILogger<ExtensionLoggingStartup> _logger;
+        private readonly IEnumerable<ExtensionInstance> _extensions;
+
+        public ExtensionLoggingStartup(ILogger<ExtensionLoggingStartup> logger, IEnumerable<ExtensionInstance> extensions)
+        {
+            _logger = logger;
+            _extensions = extensions;
+        }
+
+        public Task<bool> StartupAsync(CancellationToken token)
+        {
+            _logger.LogInformation("Loaded {0} extensions", _extensions.Count());
+
+            foreach (var extension in _extensions)
+            {
+                _logger.LogDebug("Loaded extension: {Name} [{Location}]", extension.Name, extension.Location);
+            }
+
+            return Task.FromResult(true);
+        }
+    }
+}
--- src/components/Microsoft.DotNet.UpgradeAssistant.Extensions/ExtensionProviderExtensions.cs
+++ src/components/Microsoft.DotNet.UpgradeAssistant.Extensions/ExtensionProviderExtensions.cs
@@ -7,8 +7,12 @@ using System.IO;
 using System.Linq;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Logging;
 
 namespace Microsoft.DotNet.UpgradeAssistant.Extensions
 {
@@ -36,6 +40,7 @@ namespace Microsoft.DotNet.UpgradeAssistant.Extensions
                 throw new ArgumentNullException(nameof(configuration));
             }
 
+            services.AddTransient<IUpgradeStartup, ExtensionLoggingStartup>();
             services.AddSerializer();
 
             foreach (var extension in GetExtensions(configuration, additionalExtensionPaths))
