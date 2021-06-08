29580acdc583dd2f984475dacbd677cb49292b28
Taylor Southwick <tasou@microsoft.com>
2021-06-08 11:53:53 -0700

    Add better support for zip extensions (#607)
    - AssemblyLoadContext requires assemblies to be seekable, so we now will copy assemblies to byte arrays if they are not seekable
    - We check for pattern that occurs when a zip file is created from a folder where the top level directory is a simple folder
    
    This change also consolidates loading logic all into the assembly load context where it will attempt to load the assemblies from the manifest.

--- src/components/Microsoft.DotNet.UpgradeAssistant.Extensions/ExtensionMappedFileConfigureOptions{TOption,TTo}.cs
+++ src/components/Microsoft.DotNet.UpgradeAssistant.Extensions/ExtensionMappedFileConfigureOptions{TOption,TTo}.cs
@@ -82,26 +82,5 @@ namespace Microsoft.DotNet.UpgradeAssistant.Extensions
                 return Array.Empty<TTo>();
             }
         }
-
-        private class SubFileProvider : IFileProvider
-        {
-            private readonly IFileProvider _other;
-            private readonly string _path;
-
-            public SubFileProvider(IFileProvider other, string path)
-            {
-                _other = other;
-                _path = path;
-            }
-
-            public IDirectoryContents GetDirectoryContents(string subpath)
-                => _other.GetDirectoryContents(Path.Combine(_path, subpath));
-
-            public IFileInfo GetFileInfo(string subpath)
-                => _other.GetFileInfo(Path.Combine(_path, subpath));
-
-            public IChangeToken Watch(string filter)
-                => _other.Watch(Path.Combine(_path, filter));
-        }
     }
 }
