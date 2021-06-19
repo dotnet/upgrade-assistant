7d44f82348c13f213347c03c3f8e341b36d03d47
Taylor Southwick <tasou@microsoft.com>
2021-06-18 17:54:41 -0700

    Split for + when parsing version

--- src/cli/Microsoft.DotNet.UpgradeAssistant.Cli/Constants.cs
+++ src/cli/Microsoft.DotNet.UpgradeAssistant.Cli/Constants.cs
@@ -13,24 +13,25 @@ namespace Microsoft.DotNet.UpgradeAssistant.Cli
             get
             {
                 var attribute = typeof(Constants).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
-                return attribute?.InformationalVersion ?? "0.0.0-unspecified";
+                return attribute!.InformationalVersion;
             }
         }
 
-        public static Version Version
+        public static Version Version => Version.Parse(GetVersionFromInformationalVersion(FullVersion));
+
+        /// <summary>
+        /// Need to grab just the version part of strings such as: <c>0.2.231602+ef31c22633e629fac65d9f9d8bf700119c888380</c> and <c>0.2.0-dev</c>.
+        /// </summary>
+        /// <param name="informational">An informational version string.</param>
+        /// <returns>A truncated version.</returns>
+        private static string GetVersionFromInformationalVersion(string informational)
         {
-            get
-            {
-                var version = FullVersion;
-                var idx = version.IndexOf('-', StringComparison.Ordinal);
+            var plusIdx = informational.IndexOf('+', StringComparison.Ordinal);
+            var minusIdx = informational.IndexOf('-', StringComparison.Ordinal);
 
-                if (idx > 0)
-                {
-                    version = version.Substring(0, idx);
-                }
+            var idx = Math.Min(plusIdx, minusIdx);
 
-                return Version.Parse(version);
-            }
+            return idx > 0 ? informational.Substring(0, idx) : informational;
         }
     }
 }
