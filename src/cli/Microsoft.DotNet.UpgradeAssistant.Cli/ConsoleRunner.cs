ecfc7757caf15876f9ef08e91abcb1c2f8a19425
Taylor Southwick <tasou@microsoft.com>
2021-07-12 15:20:22 -0700

    Add initial extension management command infrastructrue (#704)

--- src/cli/Microsoft.DotNet.UpgradeAssistant.Cli/ConsoleRunner.cs
+++ src/cli/Microsoft.DotNet.UpgradeAssistant.Cli/ConsoleRunner.cs
@@ -49,7 +49,7 @@ namespace Microsoft.DotNet.UpgradeAssistant.Cli
 
                 using var scope = _services.GetAutofacRoot().BeginLifetimeScope(builder =>
                 {
-                    foreach (var extension in _services.GetRequiredService<IEnumerable<ExtensionInstance>>())
+                    foreach (var extension in _services.GetRequiredService<IExtensionManager>().Instances)
                     {
                         var services = new ServiceCollection();
                         extension.AddServices(services);
