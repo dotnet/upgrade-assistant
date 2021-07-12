54977ba9a296c92c2bf1295d030e7a53eca960b6
Sweekriti Satpathy <swsat@microsoft.com>
2021-07-12 14:03:45 -0700

    extension files and folder for test data

---
+++ src/extensions/maui/Microsoft.DotNet.UpgradeAssistant.Extensions.Maui/MauiServiceProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Microsoft.DotNet.UpgradeAssistant.Extensions.Maui
+{
+    public class MauiServiceProvider : IExtensionServiceProvider
+    {
+        public void AddServices(IExtensionServiceCollection services)
+        {
+            if (services is null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+        }
+    }
+}
