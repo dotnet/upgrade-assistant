4505556716d91797bcced7d2b384276b07f8b5ad
Sweekriti Satpathy <swsat@microsoft.com>
2021-07-15 10:03:49 -0700

    removes tests stuff, will add in future PR

--- tests/tool/Integration.Tests/E2ETest.cs
+++ tests/tool/Integration.Tests/E2ETest.cs
@@ -40,8 +40,6 @@ namespace Integration.Tests
         [InlineData("WebLibrary/csharp", "WebLibrary.csproj", "")]
         [InlineData("WpfSample/csharp", "BeanTrader.sln", "BeanTraderClient.csproj")]
         [InlineData("WpfSample/vb", "WpfApp1.sln", "")]
-        [InlineData("MauiSample", "EwDavidForms.sln", "EwDavidForms.Android.csproj")]
-        [InlineData("MauiSample", "EwDavidForms.sln", "EwDavidForms.iOS.csproj")]
         [Theory]
         public async Task UpgradeTest(string scenarioPath, string inputFileName, string entrypoint)
         {
