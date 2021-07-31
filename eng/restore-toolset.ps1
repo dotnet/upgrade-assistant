7cd07c658ae4eb95e5fa2e6aa67da5090cff472b
Sweekriti Satpathy <swsat@microsoft.com>
2021-07-30 17:09:32 -0700

    adds sdk version to workload install

--- eng/restore-toolset.ps1
+++ eng/restore-toolset.ps1
@@ -1,5 +1,8 @@
 try {
-    dotnet workload install microsoft-android-sdk-full microsoft-ios-sdk-full --verbosity diag
+    $dotnetSDKVersion = $GlobalJson.tools.dotnet
+    dotnet --version
+    Write-Host 'dotnetRoot check' Join-Path $dotnetRoot "sdk\$dotnetSDKVersion"
+    dotnet --sdk-version $dotnetSDKVersion workload install microsoft-android-sdk-full microsoft-ios-sdk-full
 }
 catch {
   Write-Host $_.ScriptStackTrace
