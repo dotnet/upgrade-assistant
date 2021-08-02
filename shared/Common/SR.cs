04fca2c33eea0a8b9399ca2ef03bc55c2b4fed5a
Sweekriti Satpathy <swsat@microsoft.com>
2021-08-02 14:59:38 -0700

    formatting error fixed

--- shared/Common/SR.cs
+++ shared/Common/SR.cs
@@ -1,7 +1,5 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-
-
 namespace System
 {
     internal static class SR
@@ -11,7 +9,7 @@ namespace System
 
         public static string Format(string str, object? arg0, object? arg1)
            => string.Format(System.Globalization.CultureInfo.InvariantCulture, str, arg0, arg1);
-           
+
         public static string Format(string str, object? arg0, object? arg1, object? arg2)
             => string.Format(System.Globalization.CultureInfo.InvariantCulture, str, arg0, arg1, arg2);
     }
