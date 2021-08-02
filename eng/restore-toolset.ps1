0d7aa55bbed04221c754b370e82f2f900cf217f6
Sweekriti Satpathy <swsat@microsoft.com>
2021-08-02 09:46:40 -0700

    fixing syntax

--- eng/restore-toolset.ps1
+++ eng/restore-toolset.ps1
@@ -2,7 +2,7 @@ try {
     $dotnetSDKVersion = $GlobalJson.tools.dotnet
     # $dotnetRoot = Join-Path $RepoRoot '.dotnet'
     # Write-Host 'dotnetRoot check' Join-Path $dotnetRoot "\sdk\$dotnetSDKVersion"
-    dotnet --sdk-version $dotnetSDKVersion workload install microsoft-android-sdk-full microsoft-ios-sdk-full
+    dotnet workload install microsoft-android-sdk-full microsoft-ios-sdk-full --sdk-version $dotnetSDKVersion
 }
 catch {
   Write-Host $_.ScriptStackTrace
