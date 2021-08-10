2f806bf127c7b2bc148af8df0e09a73da1024d27
Sunanda Balasubramanian <subal@microsoft.com>
2021-08-10 10:28:59 -0700

    PR feedback

--- tests/steps/Microsoft.DotNet.UpgradeAssistant.Steps.Source.Tests/DiagnosticAnalysisRunnerTests.cs
+++ tests/steps/Microsoft.DotNet.UpgradeAssistant.Steps.Source.Tests/DiagnosticAnalysisRunnerTests.cs
@@ -12,9 +12,6 @@ using Autofac.Extras.Moq;
 using AutoFixture;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
-using Microsoft.CodeAnalysis.MSBuild;
-using Microsoft.CodeAnalysis.Text;
-using Microsoft.DotNet.UpgradeAssistant.Extensions.Default.Analyzers;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Xunit;
@@ -24,15 +21,6 @@ namespace Microsoft.DotNet.UpgradeAssistant.Steps.Source.Tests
     public class DiagnosticAnalysisRunnerTests
     {
         private readonly Fixture _fixture;
-        internal const string TestProjectPath = @"assets\TestProject.csproj";
-
-        private static IEnumerable<AdditionalText> AdditionalTexts
-        {
-            get
-            {
-                return Array.Empty<AdditionalText>();
-            }
-        }
 
         public DiagnosticAnalysisRunnerTests()
         {
@@ -50,13 +38,13 @@ namespace Microsoft.DotNet.UpgradeAssistant.Steps.Source.Tests
             project.Setup(p => p.GetRoslynProject()).Returns(() =>
             {
                 var ws = new AdhocWorkspace();
-                var name = Path.GetFileNameWithoutExtension(TestProjectPath)!;
-                return ws.AddProject(ProjectInfo.Create(ProjectId.CreateNewId(), VersionStamp.Default, name, name, "C#", filePath: TestProjectPath));
+                var name = Path.GetFileNameWithoutExtension("TestProject.csproj")!;
+                return ws.AddProject(ProjectInfo.Create(ProjectId.CreateNewId(), VersionStamp.Default, name, name, "C#", filePath: "TestProject.csproj"));
             });
 
             var context = mock.Mock<IUpgradeContext>();
             context.Setup(c => c.Projects).Returns(new[] { project.Object });
-            var diagnosticAnalysisRunner = new DiagnosticAnalysisRunner(GetAnalyzers(false), AdditionalTexts, loggerMock.Object);
+            var diagnosticAnalysisRunner = new DiagnosticAnalysisRunner(GetAnalyzers(false), Array.Empty<AdditionalText>(), loggerMock.Object);
 
             // Act
             var diagnostics = await diagnosticAnalysisRunner.GetDiagnosticsAsync(project.Object, CancellationToken.None).ConfigureAwait(false);
@@ -66,9 +54,9 @@ namespace Microsoft.DotNet.UpgradeAssistant.Steps.Source.Tests
         }
 
         [Theory]
-        [InlineData(false, 0)]
-        [InlineData(true, 3)]
-        public async Task DiagnosticsWithProjectFilesTest(bool includeLocation, int expected)
+        [InlineData(false, 0, null)]
+        [InlineData(true, 1, "Test1")]
+        public async Task DiagnosticsWithProjectFilesTest(bool includeLocation, int expectedDiagnosticCount, string? expectedDiagnosticId)
         {
             // Arrange
             using var mock = AutoMock.GetLoose();
@@ -78,8 +66,8 @@ namespace Microsoft.DotNet.UpgradeAssistant.Steps.Source.Tests
             project.Setup(p => p.GetRoslynProject()).Returns(() =>
             {
                 var ws = new AdhocWorkspace();
-                var name = Path.GetFileNameWithoutExtension(TestProjectPath)!;
-                var projectInfo = ProjectInfo.Create(ProjectId.CreateNewId(), VersionStamp.Default, name, name, "C#", filePath: TestProjectPath);
+                var name = Path.GetFileNameWithoutExtension("TestProject.csproj")!;
+                var projectInfo = ProjectInfo.Create(ProjectId.CreateNewId(), VersionStamp.Default, name, name, "C#", filePath: "TestProject.csproj");
                 var documents = new List<DocumentInfo>() { DocumentInfo.Create(DocumentId.CreateNewId(projectInfo.Id), "Foo.cs").WithFilePath(@"assets\TestClasses\Foo.cs") };
                 var proj = ws.AddProject(projectInfo.WithDocuments(documents));
                 return proj;
@@ -87,31 +75,24 @@ namespace Microsoft.DotNet.UpgradeAssistant.Steps.Source.Tests
 
             var context = mock.Mock<IUpgradeContext>();
             context.Setup(c => c.Projects).Returns(new[] { project.Object });
-            var diagnosticAnalysisRunner = new DiagnosticAnalysisRunner(GetAnalyzers(includeLocation), AdditionalTexts, loggerMock.Object);
+            var diagnosticAnalysisRunner = new DiagnosticAnalysisRunner(GetAnalyzers(includeLocation), Array.Empty<AdditionalText>(), loggerMock.Object);
 
             // Act
             var diagnostics = await diagnosticAnalysisRunner.GetDiagnosticsAsync(project.Object, CancellationToken.None).ConfigureAwait(false);
 
             // Assert
-            Assert.True(diagnostics.Count() == expected);
+            Assert.True(diagnostics.Count() == expectedDiagnosticCount);
+            if (diagnostics.Any())
+            {
+                Assert.Collection(diagnostics, r => Assert.Equal(r.Id, expectedDiagnosticId));
+            }
         }
 
         private Mock<IProject> CreateProject(AutoMock mock)
         {
             var project = mock.Mock<IProject>();
-            var nugetReferences = mock.Mock<INuGetReferences>();
-            nugetReferences.Setup(n => n.IsTransitivelyAvailableAsync(It.IsAny<string>(), default))
-                .Returns(new ValueTask<bool>(false));
             var projectFile = mock.Mock<IProjectFile>();
-            projectFile.Setup(f => f.IsSdk).Returns(true);
-            projectFile.Setup(f => f.Sdk).Returns(new HashSet<string>(new[] { "Microsoft.NET.Sdk.Web" }));
-            projectFile.Setup(f => f.GetPropertyValue(It.IsAny<string>())).Returns(string.Empty);
-
             project.Setup(p => p.GetFile()).Returns(projectFile.Object);
-            project.Setup(p => p.TargetFrameworks).Returns(new[] { TargetFrameworkMoniker.Net50 });
-            project.Setup(p => p.GetComponentsAsync(default)).Returns(new ValueTask<ProjectComponents>(ProjectComponents.AspNetCore));
-            project.Setup(p => p.OutputType).Returns(ProjectOutputType.Exe);
-            project.Setup(p => p.NuGetReferences).Returns(nugetReferences.Object);
             project.Setup(p => p.FileInfo).Returns(new FileInfo(_fixture.Create<string>()));
 
             return project;
@@ -119,22 +100,17 @@ namespace Microsoft.DotNet.UpgradeAssistant.Steps.Source.Tests
 
         private IEnumerable<DiagnosticAnalyzer> GetAnalyzers(bool includeLocation)
         {
-            var analyzers = new List<DiagnosticAnalyzer>();
-            for (int i = 0; i < 3; i++)
+            var analyzer = new Mock<DiagnosticAnalyzer>();
+            var descriptor = new DiagnosticDescriptor($"Test1", $"Test diagnostic 1", $"Test message 1", "Test", DiagnosticSeverity.Warning, true);
+            analyzer.Setup(a => a.SupportedDiagnostics).Returns(ImmutableArray.Create(descriptor));
+            analyzer.Setup(a => a.Initialize(It.IsAny<AnalysisContext>())).Callback<AnalysisContext>(context => context.RegisterSyntaxTreeAction(x =>
             {
-                var analyzer = new Mock<DiagnosticAnalyzer>();
-                var descriptor = new DiagnosticDescriptor($"Test{i}", $"Test diagnostic {i}", $"Test message {i}", "Test", DiagnosticSeverity.Warning, true);
-                analyzer.Setup(a => a.SupportedDiagnostics).Returns(ImmutableArray.Create(descriptor));
-                analyzer.Setup(a => a.Initialize(It.IsAny<AnalysisContext>())).Callback<AnalysisContext>(context => context.RegisterSyntaxTreeAction(x =>
-                {
-                    var location = includeLocation ? x.Tree.GetRoot().GetLocation() : null;
-                    var diagnostic = Diagnostic.Create(descriptor, location);
-                    x.ReportDiagnostic(diagnostic);
-                }));
-                analyzers.Add(analyzer.Object);
-            }
+                var location = includeLocation ? x.Tree.GetRoot().GetLocation() : null;
+                var diagnostic = Diagnostic.Create(descriptor, location);
+                x.ReportDiagnostic(diagnostic);
+            }));
 
-            return analyzers;
+            return new List<DiagnosticAnalyzer>() { analyzer.Object };
         }
     }
 }
