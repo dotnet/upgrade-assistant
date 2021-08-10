98f8f67b8a9ec55a05010b104bbcf18b4f2c7246
Sunanda Balasubramanian <subal@microsoft.com>
2021-08-10 15:18:02 -0700

    Add AnalysisProvider for diagnostic analyzers (#764)
    * Add AnalysisProvider for diagnostic analyzers
    
    * PR feedback Round 1
    
    * Use diagnostic id for rule id
    
    * Restructuring AnalyzeResult to better support Diagnostic Analyzers
    
    * PR feedback
    
    * Removing the component based filter
    
    * Fix build break
    
    * Moving common diagnostic changes to separate class
    
    * Change SourceUpdaterStep and CodeFixerStep to use the separate Diagnostic analysis service
    
    * Add missing await for refreshdiagnostics call
    
    * Add some basic tests
    
    * PR feedback
    
    * Unblock tests
    
    * A few renames
    
    * Remove the test file as it is not relevant to the test anymore
    
    * Minor fixes and renames

--- src/common/Microsoft.DotNet.UpgradeAssistant.Abstractions/AnalyzeResult.cs
+++ src/common/Microsoft.DotNet.UpgradeAssistant.Abstractions/AnalyzeResult.cs
@@ -1,15 +1,20 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
 using System.Collections.Generic;
 
 namespace Microsoft.DotNet.UpgradeAssistant.Analysis
 {
     public record AnalyzeResult
     {
+        public string RuleId { get; init; } = string.Empty;
+
+        public string RuleName { get; init; } = string.Empty;
+
+        public int LineNumber { get; init; }
+
         public string FileLocation { get; init; } = string.Empty;
 
-        public HashSet<string> Results { get; init; } = new HashSet<string>();
+        public string ResultMessage { get; init; } = string.Empty;
     }
 }
