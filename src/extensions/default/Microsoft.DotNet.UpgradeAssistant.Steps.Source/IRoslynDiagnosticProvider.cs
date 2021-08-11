3adc24188c17026b245adf99a830eca4f1078592
Taylor Southwick <tasou@microsoft.com>
2021-08-10 17:20:12 -0700

    Refactor code fixers into IRoslynDiagnosticProvider

--- src/extensions/default/Microsoft.DotNet.UpgradeAssistant.Steps.Source/IRoslynDiagnosticProvider.cs
+++ src/extensions/default/Microsoft.DotNet.UpgradeAssistant.Steps.Source/IRoslynDiagnosticProvider.cs
@@ -1,18 +1,20 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.CodeFixes;
 
 namespace Microsoft.DotNet.UpgradeAssistant.Steps.Source
 {
     public interface IRoslynDiagnosticProvider
     {
         Task<IEnumerable<Diagnostic>> GetDiagnosticsAsync(IProject project, CancellationToken token);
+
+        IEnumerable<CodeFixProvider> GetCodeFixProviders();
+
+        IEnumerable<DiagnosticDescriptor> GetDiagnosticDescriptors(CodeFixProvider provider);
     }
 }
