5cfbe87bb072702bf6a194eabcd29834a957e583
Sweekriti Satpathy <swsat@microsoft.com>
2021-08-18 12:43:29 -0700

    experiment without ci flag and no versions.prop

--- eng/restore-toolset.ps1
+++ eng/restore-toolset.ps1
@@ -4,9 +4,9 @@ try {
   dotnet workload install maui --verbosity diag --temp-dir $dotnetRoot
 
   dotnet tool install -g Redth.Net.Maui.Check
-  maui-check --main --force-dotnet --ci --non-interactive --fix --skip androidsdk --skip xcode --skip vswin --skip vsmac --skip edgewebview2
+  maui-check --main --force-dotnet --non-interactive --fix --skip androidsdk --skip xcode --skip vswin --skip vsmac --skip edgewebview2
 
-  maui-check --main --force-dotnet --ci --non-interactive --fix --skip androidsdk --skip xcode --skip vswin --skip vsmac --skip edgewebview2
+  maui-check --main --force-dotnet --non-interactive --fix --skip androidsdk --skip xcode --skip vswin --skip vsmac --skip edgewebview2
 }
 catch {
   Write-Host $_.ScriptStackTrace
