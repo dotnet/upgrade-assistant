7b2c5314bd3cacfabe307617e74a5d14da41834c
Sweekriti Satpathy <swsat@microsoft.com>
2021-08-18 15:03:58 -0700

    rearrange workload install order

--- eng/restore-toolset.ps1
+++ eng/restore-toolset.ps1
@@ -4,9 +4,10 @@ try {
   # dotnet tool install -g Redth.Net.Maui.Check
   # maui-check --ci --non-interactive --fix --skip androidsdk --skip xcode --skip vswin --skip vsmac --skip edgewebview2
   # dotnet workload install android-aot ios maccatalyst tizen maui tvos macos --skip-manifest-update --no-cache --disable-parallel --verbosity diag --temp-dir $dotnetRoot
+  dotnet workload install maui --verbosity diag --temp-dir $dotnetRoot
   dotnet workload install android-aot --verbosity diag --temp-dir $dotnetRoot
   dotnet workload install ios --verbosity diag --temp-dir $dotnetRoot
-  dotnet workload install maui --verbosity diag --temp-dir $dotnetRoot
+  
 }
 catch {
   Write-Host $_.ScriptStackTrace
