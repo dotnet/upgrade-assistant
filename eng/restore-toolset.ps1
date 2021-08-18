6d5e97374690e701190a3fac9c6a9136b600fde5
Sweekriti Satpathy <swsat@microsoft.com>
2021-08-18 15:13:01 -0700

    added overrides

--- eng/restore-toolset.ps1
+++ eng/restore-toolset.ps1
@@ -1,12 +1,16 @@
 $dotnetRoot = Join-Path $RepoRoot '.dotnet'
+$dotnetSdkVersion = $GlobalJson.tools.dotnet
 
 try {
   # dotnet tool install -g Redth.Net.Maui.Check
   # maui-check --ci --non-interactive --fix --skip androidsdk --skip xcode --skip vswin --skip vsmac --skip edgewebview2
   # dotnet workload install android-aot ios maccatalyst tizen maui tvos macos --skip-manifest-update --no-cache --disable-parallel --verbosity diag --temp-dir $dotnetRoot
-  dotnet workload install maui --verbosity diag --temp-dir $dotnetRoot
-  dotnet workload install android-aot --verbosity diag --temp-dir $dotnetRoot
-  dotnet workload install ios --verbosity diag --temp-dir $dotnetRoot
+
+  dotnet workload install maui --sdk-version $dotnetSdkVersion --disable-parallel --verbosity diag --temp-dir $dotnetRoot
+  dotnet workload repair --sdk-version $dotnetSdkVersion
+  dotnet workload update --sdk-version $dotnetSdkVersion
+  dotnet workload install android-aot --sdk-version $dotnetSdkVersion --disable-parallel --verbosity diag --temp-dir $dotnetRoot
+  dotnet workload install ios --sdk-version $dotnetSdkVersion --disable-parallel --verbosity diag --temp-dir $dotnetRoot
   
 }
 catch {
