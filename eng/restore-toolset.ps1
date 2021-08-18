460bb6a96abd9879b17afb26f7a9c66fbfcca020
Sunanda Balasubramanian <subal@microsoft.com>
2021-08-18 10:11:00 -0700

    Skipping maui tests to unblock merge

--- eng/restore-toolset.ps1
+++ eng/restore-toolset.ps1
@@ -1,13 +1,7 @@
 $dotnetRoot = Join-Path $RepoRoot '.dotnet'
-$dotnetSdkVersion = $GlobalJson.tools.dotnet
 
 try {
-  
     dotnet workload install maui --verbosity diag --temp-dir $dotnetRoot
-    dotnet workload repair 
-    dotnet workload update --sdk-version $dotnetSdkVersion --no-cache --disable-parallel
-    dotnet workload install --sdk-version $dotnetSdkVersion --no-cache --disable-parallel android-aot ios maui wasm-tools --skip-manifest-update --verbosity diag --temp-dir $dotnetRoot
-
 }
 catch {
   Write-Host $_.ScriptStackTrace
