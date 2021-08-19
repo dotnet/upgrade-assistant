49dd72eed32ea088fceb9591a985bdb2e5513fa8
Sweekriti Satpathy <swsat@microsoft.com>
2021-08-18 17:49:18 -0700

    only maui check

--- eng/restore-toolset.ps1
+++ eng/restore-toolset.ps1
@@ -3,14 +3,14 @@ $dotnetSdkVersion = $GlobalJson.tools.dotnet
 
 try {
   
-  dotnet workload install maui --disable-parallel --verbosity diag --temp-dir $dotnetRoot
-  dotnet workload install android --disable-parallel --verbosity diag --temp-dir $dotnetRoot
-  dotnet workload install ios --disable-parallel --verbosity diag --temp-dir $dotnetRoot
+  # dotnet workload install maui --disable-parallel --verbosity diag --temp-dir $dotnetRoot
+  # dotnet workload install android --disable-parallel --verbosity diag --temp-dir $dotnetRoot
+  # dotnet workload install ios --disable-parallel --verbosity diag --temp-dir $dotnetRoot
 
   dotnet tool install -g Redth.Net.Maui.Check
-  maui-check --main --ci --non-interactive --fix --skip androidsdk --skip xcode --skip vswin --skip vsmac --skip edgewebview2
+  maui-check --ci --non-interactive --fix --skip androidsdk --skip xcode --skip vswin --skip vsmac --skip edgewebview2
 
-  maui-check --main --ci --non-interactive --fix --skip androidsdk --skip xcode --skip vswin --skip vsmac --skip edgewebview2
+  # maui-check --main --ci --non-interactive --fix --skip androidsdk --skip xcode --skip vswin --skip vsmac --skip edgewebview2
 
 
   # dotnet workload install maui --sdk-version $dotnetSdkVersion --disable-parallel --verbosity diag --temp-dir $dotnetRoot
