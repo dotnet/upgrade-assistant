7c497d6d62f3bcfdd4beb710eb6a14c1e6019f7f
Sweekriti Satpathy <swsat@microsoft.com>
2021-08-24 16:04:47 -0700

    new way

--- eng/restore-toolset.ps1
+++ eng/restore-toolset.ps1
@@ -4,19 +4,19 @@
 try {
   # dotnet workload install maui
 
-  dotnet workload install android-aot --source "https://aka.ms/dotnet/maui/main/index.json"
-  dotnet workload install ios --source "https://aka.ms/dotnet/maui/main/index.json"
-  dotnet workload install maui --source "https://aka.ms/dotnet/maui/main/index.json"
+  # dotnet workload install android-aot --source "https://aka.ms/dotnet/maui/main/index.json"
+  # dotnet workload install ios --source "https://aka.ms/dotnet/maui/main/index.json"
+  # dotnet workload install maui --source "https://aka.ms/dotnet/maui/main/index.json"
 
   # dotnet workload install maui --disable-parallel --verbosity diag --temp-dir $dotnetRoot
   # dotnet workload install android --disable-parallel --verbosity diag --temp-dir $dotnetRoot
   # dotnet workload install ios --disable-parallel --verbosity diag --temp-dir $dotnetRoot
 
-  # dotnet tool install -g Redth.Net.Maui.Check
-  # maui-check --main --non-interactive --fix --skip androidsdk --skip xcode --skip vswin --skip vsmac --skip edgewebview2
+  dotnet tool install -g Redth.Net.Maui.Check
+  maui-check --main --force-dotnet --non-interactive --fix --skip androidsdk --skip xcode --skip vswin --skip vsmac --skip edgewebview2
 
   # maui-check --main --ci --non-interactive --fix --skip androidsdk --skip xcode --skip vswin --skip vsmac --skip edgewebview2
-
+  dotnet workload install maui --source "https://aka.ms/dotnet/maui/main/index.json"
 
   # dotnet workload install maui --sdk-version $dotnetSdkVersion --disable-parallel --verbosity diag --temp-dir $dotnetRoot
   # dotnet workload repair --sdk-version $dotnetSdkVersion
