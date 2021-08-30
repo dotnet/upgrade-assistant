af5c301990dd16790787c60866d75ec5308a3886
Sweekriti Satpathy <swsat@microsoft.com>
2021-08-30 14:31:00 -0700

    totally follows new instructions

--- eng/restore-toolset.ps1
+++ eng/restore-toolset.ps1
@@ -1,20 +1,10 @@
 $dotnetRoot = Join-Path $RepoRoot '.dotnet'
 
 try {
-  dotnet workload install maui --verbosity diag --temp-dir $dotnetRoot
-
-  # dotnet tool install -g Redth.Net.Maui.Check
-  # maui-check --main --force-dotnet --non-interactive --fix --skip androidsdk --skip xcode --skip vswin --skip vsmac --skip edgewebview2
-
-  # maui-check --main --ci --non-interactive --fix --skip androidsdk --skip xcode --skip vswin --skip vsmac --skip edgewebview2
-  # dotnet workload install maui --source "https://aka.ms/dotnet/maui/main/index.json"
-
-  # dotnet workload install maui --sdk-version $dotnetSdkVersion --disable-parallel --verbosity diag --temp-dir $dotnetRoot
-  # dotnet workload repair --sdk-version $dotnetSdkVersion
-  # dotnet workload update --sdk-version $dotnetSdkVersion
-  # dotnet workload install android-aot --sdk-version $dotnetSdkVersion --disable-parallel --verbosity diag --temp-dir $dotnetRoot
-  # dotnet workload install ios --sdk-version $dotnetSdkVersion --disable-parallel --verbosity diag --temp-dir $dotnetRoot
-  
+  dotnet workload install ios --source https://aka.ms/dotnet/maui/rc1/index.json
+  dotnet workload install android-aot --source https://aka.ms/dotnet/maui/rc1/index.json
+  dotnet workload install maccatalyst --source https://aka.ms/dotnet/maui/rc1/index.json
+  dotnet workload install maui --source https://aka.ms/dotnet/maui/rc1/index.json
 }
 catch {
   Write-Host $_.ScriptStackTrace
