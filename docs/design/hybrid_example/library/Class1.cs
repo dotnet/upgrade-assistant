c293a99ab2e5d43b1b0ebf2cc9c6101f5078063b
Taylor Southwick <tasou@microsoft.com>
2021-09-10 12:03:45 -0700

    Example with webpag

--- docs/design/hybrid_example/library/Class1.cs
+++ docs/design/hybrid_example/library/Class1.cs
@@ -1,7 +1,17 @@
-﻿namespace hybrid_example
+﻿using System.Web;
+
+namespace hybrid_example
 {
     public class Class1
     {
+        public string Method(HttpContext context)
+        {
+            return Method2(context.Request);
+        }
 
+        public string Method2(HttpRequest request)
+        {
+            return request.Headers["headerName"];
+        }
     }
 }
