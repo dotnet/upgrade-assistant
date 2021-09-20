afeda1ce4d2edbc413b5638301c272e606a5131f
Taylor Southwick <tasou@microsoft.com>
2021-09-20 11:59:38 -0700

    Add output to script

--- build_extensions.ps1
+++ build_extensions.ps1
@@ -4,12 +4,14 @@ $projects = "LooseAssembly", "LooseAssembly.NuGet"
 $cli = "$PSScriptRoot\artifacts\bin\Microsoft.DotNet.UpgradeAssistant.Cli\$Configuration\net6.0\Microsoft.DotNet.UpgradeAssistant.Cli.exe"
 $packageDirectory = "$PSScriptRoot\artifacts\packages\$Configuration\Shipping"
 
+Write-Output "Moving to $packageDirectory"
 pushd $packageDirectory
 
 $env:UA_FEATURES="EXTENSION_MANAGEMENT"
 
 foreach($project in $projects){
   $path = "$PSScriptRoot\artifacts\bin\$project\$Configuration\net5.0\publish"
+  Write-Host "Building extension for $project at $path"
   & $cli extensions create $path
 }
 
