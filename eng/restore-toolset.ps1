824c26f5dcde1d8f96f02da273329089b9fe28cc
Taylor Southwick <tasou@microsoft.com>
2021-10-13 23:48:15 -0700

    ci: disable maui install

--- eng/restore-toolset.ps1
+++ eng/restore-toolset.ps1
@@ -1,7 +1,7 @@
 $dotnetRoot = Join-Path $RepoRoot '.dotnet'
 
 try {
-    dotnet workload install maui --verbosity diag --temp-dir $dotnetRoot
+    # dotnet workload install maui --verbosity diag --temp-dir $dotnetRoot
 }
 catch {
   Write-Host $_.ScriptStackTrace
