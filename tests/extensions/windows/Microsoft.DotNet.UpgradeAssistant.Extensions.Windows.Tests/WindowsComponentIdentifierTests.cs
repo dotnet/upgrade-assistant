c41511315409ac738c690ccc83759bb0aa8ba4df
Taylor Southwick <tasou@microsoft.com>
2022-01-14 09:42:49 -0800

    Fix tests

--- tests/extensions/windows/Microsoft.DotNet.UpgradeAssistant.Extensions.Windows.Tests/WindowsComponentIdentifierTests.cs
+++ tests/extensions/windows/Microsoft.DotNet.UpgradeAssistant.Extensions.Windows.Tests/WindowsComponentIdentifierTests.cs
@@ -27,13 +27,17 @@ namespace Microsoft.DotNet.UpgradeAssistant.Extensions.Windows.Tests
             // Arrange
             using var mock = AutoMock.GetLoose();
 
-            var projectFile = mock.Mock<IProjectFile>();
+            mock.Mock<ITransitiveDependencyIdentifier>()
+                .Setup(p => p.GetTransitiveDependenciesAsync(It.IsAny<IEnumerable<NuGetReference>>(), It.IsAny<IEnumerable<TargetFrameworkMoniker>>(), default))
+                .ReturnsAsync(TransitiveClosureCollection.Empty);
+
+            var projectFile = new Mock<IProjectFile>();
             projectFile.Setup(f => f.IsSdk).Returns(true);
             projectFile.Setup(f => f.Sdk).Returns(Array.Empty<string>());
             projectFile.Setup(f => f.GetPropertyValue(It.IsAny<string>())).Returns(string.Empty);
             projectFile.Setup(f => f.GetPropertyValue(propertyName)).Returns(value);
 
-            var project = mock.Mock<IProject>();
+            var project = new Mock<IProject>();
             project.Setup(p => p.GetFile()).Returns(projectFile.Object);
             project.Setup(p => p.NuGetReferences).Returns(mock.Mock<INuGetReferences>().Object);
             project.Setup(p => p.TargetFrameworks).Returns(Array.Empty<TargetFrameworkMoniker>());
@@ -56,12 +60,16 @@ namespace Microsoft.DotNet.UpgradeAssistant.Extensions.Windows.Tests
             // Arrange
             using var mock = AutoMock.GetLoose();
 
-            var projectFile = mock.Mock<IProjectFile>();
+            mock.Mock<ITransitiveDependencyIdentifier>()
+                .Setup(p => p.GetTransitiveDependenciesAsync(It.IsAny<IEnumerable<NuGetReference>>(), It.IsAny<IEnumerable<TargetFrameworkMoniker>>(), default))
+                .ReturnsAsync(TransitiveClosureCollection.Empty);
+
+            var projectFile = new Mock<IProjectFile>();
             projectFile.Setup(f => f.IsSdk).Returns(true);
             projectFile.Setup(f => f.Sdk).Returns(new HashSet<string>(sdk, StringComparer.OrdinalIgnoreCase));
             projectFile.Setup(f => f.GetPropertyValue(It.IsAny<string>())).Returns(string.Empty);
 
-            var project = mock.Mock<IProject>();
+            var project = new Mock<IProject>();
             project.Setup(p => p.GetFile()).Returns(projectFile.Object);
             project.Setup(p => p.TargetFrameworks).Returns(Array.Empty<TargetFrameworkMoniker>());
             project.Setup(p => p.NuGetReferences).Returns(mock.Mock<INuGetReferences>().Object);
@@ -95,12 +103,16 @@ namespace Microsoft.DotNet.UpgradeAssistant.Extensions.Windows.Tests
 
             using var mock = AutoMock.GetLoose();
 
-            var projectFile = mock.Mock<IProjectFile>();
+            mock.Mock<ITransitiveDependencyIdentifier>()
+                .Setup(p => p.GetTransitiveDependenciesAsync(It.IsAny<IEnumerable<NuGetReference>>(), It.IsAny<IEnumerable<TargetFrameworkMoniker>>(), default))
+                .ReturnsAsync(TransitiveClosureCollection.Empty);
+
+            var projectFile = new Mock<IProjectFile>();
             projectFile.Setup(f => f.IsSdk).Returns(true);
             projectFile.Setup(f => f.Sdk).Returns(Array.Empty<string>());
             projectFile.Setup(f => f.GetPropertyValue(It.IsAny<string>())).Returns(string.Empty);
 
-            var project = mock.Mock<IProject>();
+            var project = new Mock<IProject>();
             project.Setup(p => p.GetFile()).Returns(projectFile.Object);
             project.Setup(p => p.FrameworkReferences).Returns(frameworkReferences);
             project.Setup(p => p.TargetFrameworks).Returns(Array.Empty<TargetFrameworkMoniker>());
@@ -132,9 +144,12 @@ namespace Microsoft.DotNet.UpgradeAssistant.Extensions.Windows.Tests
 
             using var mock = AutoMock.GetLoose();
 
-            var projectFile = mock.Mock<IProjectFile>();
+            mock.Mock<ITransitiveDependencyIdentifier>()
+                .Setup(p => p.GetTransitiveDependenciesAsync(It.IsAny<IEnumerable<NuGetReference>>(), It.IsAny<IEnumerable<TargetFrameworkMoniker>>(), default))
+                .ReturnsAsync(TransitiveClosureCollection.Empty);
 
-            var project = mock.Mock<IProject>();
+            var projectFile = new Mock<IProjectFile>();
+            var project = new Mock<IProject>();
             project.Setup(p => p.GetFile()).Returns(projectFile.Object);
             project.Setup(p => p.References).Returns(references);
             project.Setup(p => p.TargetFrameworks).Returns(Array.Empty<TargetFrameworkMoniker>());
