101d1156704cceec4bfa3118979aed780524b06f
Taylor Southwick <tasou@microsoft.com>
2022-01-20 14:32:43 -0800

    add command

--- src/cli/Microsoft.DotNet.UpgradeAssistant.Cli/Commands/FeatureFlagCommand.cs
+++ src/cli/Microsoft.DotNet.UpgradeAssistant.Cli/Commands/FeatureFlagCommand.cs
@@ -14,6 +14,7 @@ using Microsoft.DotNet.UpgradeAssistant.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace Microsoft.DotNet.UpgradeAssistant.Cli
 {
@@ -22,11 +23,22 @@ namespace Microsoft.DotNet.UpgradeAssistant.Cli
         public FeatureFlagCommand()
             : base("features")
         {
-            Handler = CommandHandler.Create<ParseResult, UpgradeOptions, CancellationToken>((result, options, token) =>
+            AddOption(new Option<bool>("--list", description: "List available feature flags and status"));
+            Handler = CommandHandler.Create<ParseResult, FeatureUpgradeOptions, CancellationToken>((result, options, token) =>
              Host.CreateDefaultBuilder()
                  .UseConsoleUpgradeAssistant<Features>(options, result)
                  .ConfigureServices(services =>
                  {
+                     services.AddOptions<FeatureOption>()
+                         .Configure(opts =>
+                         {
+                             opts.Command = options switch
+                             {
+                                 { List: true } => FeatureCommand.List,
+                                 _ => FeatureCommand.None,
+                             };
+                         });
+
                      services.AddOptions<ExtensionOptions>()
                          .Configure(options =>
                          {
@@ -38,10 +50,12 @@ namespace Microsoft.DotNet.UpgradeAssistant.Cli
 
         private class Features : IAppCommand
         {
+            private readonly IOptions<FeatureOption> _options;
             private readonly ILogger<Features> _logger;
 
-            public Features(ILogger<Features> logger)
+            public Features(IOptions<FeatureOption> options, ILogger<Features> logger)
             {
+                _options = options;
                 _logger = logger;
             }
 
@@ -50,17 +64,33 @@ namespace Microsoft.DotNet.UpgradeAssistant.Cli
                 _logger.LogWarning("Features are subject to change and may be disabled in a subsequent release.");
                 _logger.LogInformation("Features are registered in the environment variable `UA_FEATURES`. Below are available features:");
 
-                foreach (var feature in FeatureFlags.RegisteredFeatures.OrderBy(static f => f))
+                if (_options.Value.Command is FeatureCommand.List)
                 {
-                    _logger.LogInformation("'{Feature}': {IsEnabled}", feature, FeatureFlags.IsRegistered(feature));
+                    foreach (var feature in FeatureFlags.RegisteredFeatures.OrderBy(static f => f))
+                    {
+                        _logger.LogInformation("'{Feature}': {IsEnabled}", feature, FeatureFlags.IsRegistered(feature));
+                    }
                 }
 
                 return Task.CompletedTask;
             }
         }
 
-        private class UpgradeOptions : IUpgradeAssistantOptions
+        private enum FeatureCommand
         {
+            None,
+            List,
+        }
+
+        private class FeatureOption
+        {
+            public FeatureCommand Command { get; set; }
+        }
+
+        private class FeatureUpgradeOptions : IUpgradeAssistantOptions
+        {
+            public bool List { get; set; }
+
             public bool Verbose { get; set; }
 
             public bool IsVerbose => Verbose;
