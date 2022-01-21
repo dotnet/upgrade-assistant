ab5afaace2f8f4e7351629d11c7e29d285fc78fa
Taylor Southwick <tasou@microsoft.com>
2022-01-21 13:07:33 -0800

    Remove extra test

--- tests/extensions/default/Microsoft.DotNet.UpgradeAssistant.Steps.Packages.Tests/TransitiveReferenceAnalyzerTests.cs
+++ tests/extensions/default/Microsoft.DotNet.UpgradeAssistant.Steps.Packages.Tests/TransitiveReferenceAnalyzerTests.cs
@@ -1,7 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
 using System.Linq;
 using Autofac.Extras.Moq;
 using Microsoft.DotNet.UpgradeAssistant.Dependencies;
@@ -15,25 +14,6 @@ namespace Microsoft.DotNet.UpgradeAssistant.Steps.Packages.Tests
     {
         [Fact]
         public async void NoReferences()
-        {
-            // Arrange
-            using var mock = AutoMock.GetLoose();
-
-            var project = new Mock<IProject>();
-            project.Setup(p => p.PackageReferences).Returns(Enumerable.Empty<NuGetReference>());
-
-            var dependencyState = new Mock<IDependencyAnalysisState>();
-            dependencyState.Setup(d => d.Packages.GetEnumerator()).Returns(Enumerable.Empty<NuGetReference>().GetEnumerator());
-
-            // Act
-            await mock.Create<TransitiveReferenceAnalyzer>().AnalyzeAsync(project.Object, dependencyState.Object, default).ConfigureAwait(false);
-
-            // Assert
-            dependencyState.Verify(d => d.Packages.Remove(It.IsAny<NuGetReference>(), It.IsAny<OperationDetails>()), Times.Never);
-        }
-
-        [Fact]
-        public async void NoReferences2()
         {
             // Arrange
             using var mock = AutoMock.GetLoose();
