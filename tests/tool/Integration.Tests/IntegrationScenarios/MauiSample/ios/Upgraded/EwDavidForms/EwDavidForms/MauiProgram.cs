21bfd306230386ca12a59cd076b57aafc5541962
Sweekriti Satpathy <swsat@microsoft.com>
2022-04-28 17:13:22 -0700

    adds new mauiprogram file

---
+++ tests/tool/Integration.Tests/IntegrationScenarios/MauiSample/ios/Upgraded/EwDavidForms/EwDavidForms/MauiProgram.cs
@@ -0,0 +1,14 @@
+﻿
+namespace EwDavidForms;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>();
+
+		return builder.Build();
+	}
+}
