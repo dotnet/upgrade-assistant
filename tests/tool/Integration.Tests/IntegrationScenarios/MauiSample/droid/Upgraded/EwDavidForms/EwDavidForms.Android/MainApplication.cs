25a9c1ba6f3949c73d4e03cbe2adb99c38ab4f43
Sweekriti Satpathy <swsat@microsoft.com>
2022-05-02 16:56:21 -0700

    Fixes MAUI tests and updates pipeline to use VS2022 Preview (#1106)
    * updates pipeline to use VS2022 Previews, fixes MAUI extension tests, fixes maui upgrade steps to match rc-2 .NET MAUI release

--- tests/tool/Integration.Tests/IntegrationScenarios/MauiSample/droid/Upgraded/EwDavidForms/EwDavidForms.Android/MainApplication.cs
+++ tests/tool/Integration.Tests/IntegrationScenarios/MauiSample/droid/Upgraded/EwDavidForms/EwDavidForms.Android/MainApplication.cs
@@ -2,15 +2,18 @@
 using Android.App;
 using Android.Runtime;
 using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
 
 namespace EwDavidForms.Android
 {
 	[Application]
-	public class MainApplication : MauiApplication<Startup>
+	public class MainApplication : MauiApplication
 	{
 		public MainApplication(IntPtr handle, JniHandleOwnership ownership)
 			: base(handle, ownership)
 		{
 		}
-	}
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
 }
