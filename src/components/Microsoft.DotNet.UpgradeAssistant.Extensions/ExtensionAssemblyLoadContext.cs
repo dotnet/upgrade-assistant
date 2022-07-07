c872ff64d149aa519b1358a34b678f3ccddd1aff
Mike Rousos <Mike@MJRousos.com>
2022-07-07 17:00:01 -0400

    Prefer to load UA assemblies from the default ALC
    This update makes it so that assemblies with
    Microsoft.DotNet.UpgradeAssistant prefixes are only loaded in
    extensions' assembly load contexts if the assemblies aren't able to be
    loaded in the default context.

--- src/components/Microsoft.DotNet.UpgradeAssistant.Extensions/ExtensionAssemblyLoadContext.cs
+++ src/components/Microsoft.DotNet.UpgradeAssistant.Extensions/ExtensionAssemblyLoadContext.cs
@@ -12,6 +12,7 @@ namespace Microsoft.DotNet.UpgradeAssistant.Extensions
 {
     internal class ExtensionAssemblyLoadContext : AssemblyLoadContext
     {
+        private const string UpgradeAssistantFilePrefix = "Microsoft.DotNet.UpgradeAssistant.";
         private const string ALC_Prefix = "UA_";
 
         private readonly ExtensionInstance _extension;
@@ -29,6 +30,14 @@ namespace Microsoft.DotNet.UpgradeAssistant.Extensions
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
 
             Unloading += _ => _logger.LogDebug("{Name} extension is unloading", Name);
+
+            // The resolving event is called if Load(AssemblyName) fails to load the assembly *and*
+            // falling back to the default ALC fails to load the assembly. In such cases, we will try to load again,
+            // this time permitting the extension to load Microsoft.DotNet.UpgradeAssistant.* assemblies (which
+            // isn't typically permitted in Load) in order to support the unlikely case that the extension
+            // ships a Microsoft.DotNet.UpgradeAssistant.* assembly that isn't part of the shared UA infrastructure.
+            // Relevant loader docs: https://docs.microsoft.com/dotnet/core/dependency-loading/loading-managed#algorithm
+            Resolving += (alc, name) => Load(name, true);
 
             Load(extension, assemblies);
         }
@@ -60,8 +69,39 @@ namespace Microsoft.DotNet.UpgradeAssistant.Extensions
             }
         }
 
-        protected override Assembly? Load(AssemblyName assemblyName)
+        // When Load is called, we will not load assemblies beginning with the prefix Microsoft.DotNet.UpgradeAssistant.*
+        // because those assemblies should typically come from the default ALC so that they're shared. This prevents
+        // an extension that accidentally has UA binaries in its binary directory from loading a different instance
+        // of a UA assembly and running into issues with types not resolving.
+        protected override Assembly? Load(AssemblyName assemblyName) => Load(assemblyName, false);
+
+        /// <summary>
+        /// Attempts to load an assembly given the assembly's name by probing with the extension's
+        /// file provider (which typically means looking in the extension's path).
+        /// If the assembly is not successfully loaded, the loader will fallback to attempting to 
+        /// load the assembly from the default assembly load context.
+        /// </summary>
+        /// <param name="assemblyName">The assembly name to load.</param>
+        /// <param name="loadUpgradeAssistantAssemblies">
+        /// True if the load should consider files beginning with UA-specific prefixes
+        /// (Microsoft.DotNet.UpgradeAssistant.*) or false if such assemblies should not be loaded
+        /// in this assembly load context.
+        /// </param>
+        /// <returns>The assembly corresponding to the given assembly name or null if none was found.</returns>
+        protected Assembly? Load(AssemblyName assemblyName, bool loadUpgradeAssistantAssemblies)
         {
+            if (assemblyName.Name is null)
+            {
+                return null;
+            }
+
+            // Don't load Microsoft.DotNet.UpgradeAssistant.* assemblies unless the caller specified that
+            // such assemblies should be loaded from extension-local file paths.
+            if (assemblyName.Name.StartsWith(UpgradeAssistantFilePrefix, StringComparison.OrdinalIgnoreCase) && !loadUpgradeAssistantAssemblies)
+            {
+                return null;
+            }
+            
             var dll = $"{assemblyName.Name}.dll";
             var dllFile = _extension.FileProvider.GetFileInfo(dll);
 
