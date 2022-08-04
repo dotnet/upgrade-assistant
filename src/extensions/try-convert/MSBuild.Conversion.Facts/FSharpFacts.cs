a9ada0e3817bbe8d4c0a4ca710e334cdbb0ec75a
Brandon H <brandonh-msft@users.noreply.github.com>
2022-08-04 11:19:01 -0700

    Copy relevant try-convert contents to UA repo (#1248)
    * moving from submodule to embedded code for try-convert; Closes #1242
    
    * removing subrepo
    
    * wiring in new locations
    
    * removing old module
    
    * don't need the try-convert tool
    
    * Updates to try-convert code to resolve SA/CA violations
    
    UA has different SA/CA rules than t-c did.
    It's pretty odd these weren't caught prior CI builds w/ this code in place,
    but nevertheless the fixes are all done and code builds locally w/o issue
    and all UTs pass.
    No logic was harmed in the making of this commit. 🤞🏻

---
+++ src/extensions/try-convert/MSBuild.Conversion.Facts/FSharpFacts.cs
@@ -0,0 +1,14 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace MSBuild.Conversion.Facts
+{
+    /// <summary>
+    /// A bunch of known values regarding legacy F# projects.
+    /// </summary>
+    public static class FSharpFacts
+    {
+        public const string FSharpTargetsPathVariableName = @"$(FSharpTargetsPath)";
+        public const string FSharpTargetsPath = @"$(MSBuildExtensionsPath32)\Microsoft\VisualStudio\v$(VisualStudioVersion)\FSharp\Microsoft.FSharp.Targets";
+    }
+}
