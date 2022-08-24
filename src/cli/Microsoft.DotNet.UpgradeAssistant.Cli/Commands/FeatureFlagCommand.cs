7759eb9451dd85d20ac83e8001e549ce74ede68d
Brandon H <brandonh-msft@users.noreply.github.com>
2022-08-24 09:28:21 -0700

    Fixing analyzebinaries command fails with p windows (#1266)
    * Fixing #1253
    
    Upgrading System.CommandLine & making required changes
    Fixing Arity specifier on platform arg for binary analysis
    
    Parsing files & directories is now failing for unknown reasons, however (issue https://github.com/dotnet/command-line-api/issues/1821 )
    
    * FileSystemInfo parse workaround
    
    To work around https://github.com/dotnet/command-line-api/issues/1821
    I am using the code from inside the cli API source code
    https://github.com/dotnet/command-line-api/blob/4dcee4d8a84a5aec7bbee0e9e112a2432e19a411/src/System.CommandLine/Binding/ArgumentConverter.StringConverters.cs#L112
    
    * remove unnecessary suppression
    
    * This can't be a record type, the DI container was losing it when we tried to pass it into the Binary Analysis ApiChecker
    
    * Don't need TargetFrameworkSelector in the BA ApiChecker anymore
    
    * Removing unused types/ctor params
    
    * solving for nullability

--- src/cli/Microsoft.DotNet.UpgradeAssistant.Cli/Commands/FeatureFlagCommand.cs
+++ src/cli/Microsoft.DotNet.UpgradeAssistant.Cli/Commands/FeatureFlagCommand.cs
@@ -1,15 +1,9 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
-using System.Collections.Generic;
 using System.CommandLine;
-using System.CommandLine.Invocation;
+using System.CommandLine.NamingConventionBinder;
 using System.CommandLine.Parsing;
-using System.IO;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 
 using Microsoft.DotNet.UpgradeAssistant.Extensions;
 using Microsoft.Extensions.DependencyInjection;
